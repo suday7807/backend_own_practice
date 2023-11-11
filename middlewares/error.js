88cb90f883d5375496869fff83163acbfded9efa
suday7807 <121724593+suday7807@users.noreply.github.com>
2023-11-11 11:16:05 +0530

    Update error.js

--- middlewares/error.js
+++ middlewares/error.js
@@ -14,7 +14,7 @@ export const errorMiddleware = (err,req,res,next)=>{
     err.statusCode = err.statusCode || 500;
 
 
-    return res.status(404).json({
+    return res.status(err.statusCode).json({
         success:false,
         message:err.message
     })
